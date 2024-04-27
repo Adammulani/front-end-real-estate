0cb5b52ae1328634a09ee5a17f38c6b516b36352
Adam Mulani <aadammulani12@gmail.com>
2024-04-27 11:12:03 +0530

    Added companies logos below hero section

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Companies.css"
+
+export const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./green-house.png" alt="" />
+        <img src="./boceele.png" alt="" />
+        <img src="./larana.png" alt="" />
+        <img src="luxuary.png" alt="" />
+      </div>
+    </section>
+  );
+}
