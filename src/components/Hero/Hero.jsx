559dd655d6ac08be944913696b3c03b90607d0dc
Adam Mulani <aadammulani12@gmail.com>
2024-04-28 10:26:30 +0530

    added animation in hero section for hero title and image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import {HiLocationMarker} from "react-icons/hi";
 import CountUp from "react-countup";
+import {motion} from 'framer-motion'
 export const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -11,10 +12,17 @@ export const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+            initial={{y:"2rem",opacity:0}}
+            animate={{y:0,opacity:1}}
+            transition={{
+              duration:2,
+              type:"spring"
+            }}
+            >
               Discover <br /> Your Dream Home <br />
               with Us
-            </h1>
+            </motion.h1>
           </div>
           <div className="flexColStart hero-des wrap-it">
             <span className="secondaryText">Finding your dream home becomes effortless</span>
@@ -56,9 +64,16 @@ export const Hero = () => {
         </div>
         {/* right section */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div 
+          initial={{x:"7rem",opacity:0}}
+          animate={{x:0,opacity:1}}
+          transition={{
+            duration:2,
+            type:"spring"
+          }}
+          className="image-container">
             <img src="./hero-image.jpg" alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
